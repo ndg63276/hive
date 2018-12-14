386df1ac486dc249955db3f0920cf4d976558939
ndg63276 <mark.williams@diamond.ac.uk>
2018-12-14 12:36:14 +0000

    Make it work in python3

--- serve.py
+++ serve.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
- 
-import BaseHTTPServer
-import CGIHTTPServer
+
+import sys
+if sys.version_info[0] == 3: # python3
+
+	from http.server import HTTPServer, CGIHTTPRequestHandler
+else: # python2
+	from BaseHTTPServer import HTTPServer
+	from CGIHTTPServer import CGIHTTPRequestHandler
 import cgitb; cgitb.enable()  ## This line enables CGI error reporting
  
-server = BaseHTTPServer.HTTPServer
-handler = CGIHTTPServer.CGIHTTPRequestHandler
+server = HTTPServer
+handler = CGIHTTPRequestHandler
 server_address = ("", 8000)
 handler.cgi_directories = ["/cgi-bin"]
  
