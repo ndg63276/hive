094635ffc748fc3e27bd8af160025cb16d1febd2
ndg63276 <mark.williams@diamond.ac.uk>
2019-02-05 11:39:04 +0000

    Fix indentation

--- cgi-bin/hive.py
+++ cgi-bin/hive.py
@@ -48,7 +48,7 @@ def get_location_id():
 			min_dist_id = location['id']
 	j['location_id'] = min_dist_id
 	with open('cgi-bin/credentials.json','w') as f:
-	        json.dump(j,f, sort_keys=True, indent=4)
+		json.dump(j,f, sort_keys=True, indent=4)
 	return min_dist_id
 
 def get_weather(startdate, enddate):
@@ -74,44 +74,44 @@ def get_weather(startdate, enddate):
 	return to_return
 
 def login():
-        headers = {'Content-Type': 'application/vnd.alertme.zoo-6.1+json', 'Accept': 'application/vnd.alertme.zoo-6.1+json', 'X-Omnia-Client': 'Hive Web Dashboard'}
-        payload = {'sessions':[{'username':username,'password':password,'caller':'WEB'}]}
-        data = json.dumps(payload)
-        r = requests.post(url+'/auth/sessions', headers=headers, data=data)
-        headers['X-Omnia-Access-Token'] = r.json()['sessions'][0]['sessionId']
-        return headers
+	headers = {'Content-Type': 'application/vnd.alertme.zoo-6.1+json', 'Accept': 'application/vnd.alertme.zoo-6.1+json', 'X-Omnia-Client': 'Hive Web Dashboard'}
+	payload = {'sessions':[{'username':username,'password':password,'caller':'WEB'}]}
+	data = json.dumps(payload)
+	r = requests.post(url+'/auth/sessions', headers=headers, data=data)
+	headers['X-Omnia-Access-Token'] = r.json()['sessions'][0]['sessionId']
+	return headers
 
 def get_id(headers):
-        r=requests.get(url+'/nodes',headers=headers)
-        nodes=r.json()['nodes']
-        for i in range(len(nodes)):
-                if nodes[i]['name'] == 'Receiver 2':
-                        id=nodes[i]['id']
-        return id
+	r=requests.get(url+'/nodes',headers=headers)
+	nodes=r.json()['nodes']
+	for i in range(len(nodes)):
+		if nodes[i]['name'] == 'Receiver 2':
+			id=nodes[i]['id']
+	return id
 
 def get_temps(headers, id, startdate, enddate=None):
-        start = str(int(unix_time_millis(startdate)))
-        if enddate is None:
-               end = str(int(time.time()*1000))
-        else:
-               end = str(int(unix_time_millis(enddate)))
-        params={'start':start, 'end':end, 'timeUnit':'MINUTES', 'rate':'5', 'operation':'MAX'}
-        r=requests.get(url+'/channels/temperature@'+id+',targetTemperature@'+id, headers=headers, params=params)
-        temps=r.json()['channels'][0]['values']
-        targetTemps = r.json()['channels'][1]['values']
-        return temps, targetTemps
+	start = str(int(unix_time_millis(startdate)))
+	if enddate is None:
+		end = str(int(time.time()*1000))
+	else:
+		end = str(int(unix_time_millis(enddate)))
+	params={'start':start, 'end':end, 'timeUnit':'MINUTES', 'rate':'5', 'operation':'MAX'}
+	r=requests.get(url+'/channels/temperature@'+id+',targetTemperature@'+id, headers=headers, params=params)
+	temps=r.json()['channels'][0]['values']
+	targetTemps = r.json()['channels'][1]['values']
+	return temps, targetTemps
 
 def get_current_temps(headers, id):
-        startdate = datetime.now()-timedelta(minutes=60)
-        start = str(int(unix_time_millis(startdate)))
-        end = str(int(time.time()*1000))
-        params={'start':start, 'end':end, 'timeUnit':'SECONDS', 'rate':'10', 'operation':'MAX'}
-        r=requests.get(url+'/channels/temperature@'+id+',targetTemperature@'+id, headers=headers, params=params)
-        temps=r.json()['channels'][0]['values']
-        targetTemps = r.json()['channels'][1]['values']
-        currentTemp = round(temps[sorted(temps.keys())[-1]],2)
-        currentTarget = round(targetTemps[sorted(targetTemps.keys())[-1]],1)
-        return currentTemp, currentTarget
+	startdate = datetime.now()-timedelta(minutes=60)
+	start = str(int(unix_time_millis(startdate)))
+	end = str(int(time.time()*1000))
+	params={'start':start, 'end':end, 'timeUnit':'SECONDS', 'rate':'10', 'operation':'MAX'}
+	r=requests.get(url+'/channels/temperature@'+id+',targetTemperature@'+id, headers=headers, params=params)
+	temps=r.json()['channels'][0]['values']
+	targetTemps = r.json()['channels'][1]['values']
+	currentTemp = round(temps[sorted(temps.keys())[-1]],2)
+	currentTarget = round(targetTemps[sorted(targetTemps.keys())[-1]],1)
+	return currentTemp, currentTarget
 
 headers = login()
 id = get_id(headers)
@@ -123,68 +123,68 @@ fs = cgi.FieldStorage()
 
 startdate = datetime.now()-timedelta(days=1)
 if fs.has_key('start'):
-        start = fs.getvalue('start')
-        if start.startswith('20'):
-                startyear=int(start[0:4])
-                startmonth=int(start[4:6])
-                startday=int(start[6:8])
-                starthour = startmin = startsec = 0
-                if len(start)>8:
-                        starthour = int(start[8:10])
-                        if len(start)>10:
-                                startmin = int(start[10:12])
-                                if len(start)>12:
-                                        startsec = int(start[12:14])
-                startdate = datetime(startyear, startmonth, startday, starthour, startmin, startsec)
-        match = re.match(r'-([0-9]+)(year|month|week|day|hour|min|sec)', start, re.I)
-        if match:
-                if 'year' in match.group(2):
-                        startdate = datetime.now() - timedelta(days=int(match.group(1))*365)
-                if 'month' in match.group(2):
-                        startdate = datetime.now() - timedelta(days=int(match.group(1))*31)
-                if 'week' in match.group(2):
-                        startdate = datetime.now() - timedelta(weeks=int(match.group(1)))
-                if 'day' in match.group(2):
-                        startdate = datetime.now() - timedelta(days=int(match.group(1)))
-                if 'hour' in match.group(2):
-                        startdate = datetime.now() - timedelta(hours=int(match.group(1)))
-                if 'min' in match.group(2):
-                        startdate = datetime.now() - timedelta(minutes=int(match.group(1)))
-                if 'sec' in match.group(2):
-                        startdate = datetime.now() - timedelta(seconds=int(match.group(1)))
+	start = fs.getvalue('start')
+	if start.startswith('20'):
+		startyear=int(start[0:4])
+		startmonth=int(start[4:6])
+		startday=int(start[6:8])
+		starthour = startmin = startsec = 0
+		if len(start)>8:
+			starthour = int(start[8:10])
+			if len(start)>10:
+				startmin = int(start[10:12])
+				if len(start)>12:
+					startsec = int(start[12:14])
+		startdate = datetime(startyear, startmonth, startday, starthour, startmin, startsec)
+	match = re.match(r'-([0-9]+)(year|month|week|day|hour|min|sec)', start, re.I)
+	if match:
+		if 'year' in match.group(2):
+			startdate = datetime.now() - timedelta(days=int(match.group(1))*365)
+		if 'month' in match.group(2):
+			startdate = datetime.now() - timedelta(days=int(match.group(1))*31)
+		if 'week' in match.group(2):
+			startdate = datetime.now() - timedelta(weeks=int(match.group(1)))
+		if 'day' in match.group(2):
+			startdate = datetime.now() - timedelta(days=int(match.group(1)))
+		if 'hour' in match.group(2):
+			startdate = datetime.now() - timedelta(hours=int(match.group(1)))
+		if 'min' in match.group(2):
+			startdate = datetime.now() - timedelta(minutes=int(match.group(1)))
+		if 'sec' in match.group(2):
+			startdate = datetime.now() - timedelta(seconds=int(match.group(1)))
 
 
 enddate = datetime.now()
 if fs.has_key('end'):
-        end = fs.getvalue('end')
-        if end.startswith('20'):
-                endyear=int(end[0:4])
-                endmonth=int(end[4:6])
-                endday=int(end[6:8])
-                endhour = endmin = endsec = 0
-                if len(end)>8:
-                        endhour = int(end[8:10])
-                        if len(end)>10:
-                                endmin = int(end[10:12])
-                                if len(end)>12:
-                                        endsec = int(end[12:14])
-                enddate = datetime(endyear, endmonth, endday, endhour, endmin, endsec)
-        match = re.match(r'-([0-9]+)(year|month|week|day|hour|min|sec)', end, re.I)
-        if match:
-                if 'year' in match.group(2):
-                        enddate = datetime.now() - timedelta(days=int(match.group(1))*365)
-                if 'month' in match.group(2):
-                        enddate = datetime.now() - timedelta(days=int(match.group(1))*31)
-                if 'week' in match.group(2):
-                        enddate = datetime.now() - timedelta(weeks=int(match.group(1)))
-                if 'day' in match.group(2):
-                        enddate = datetime.now() - timedelta(days=int(match.group(1)))
-                if 'hour' in match.group(2):
-                        enddate = datetime.now() - timedelta(hours=int(match.group(1)))
-                if 'min' in match.group(2):
-                        enddate = datetime.now() - timedelta(minutes=int(match.group(1)))
-                if 'sec' in match.group(2):
-                        enddate = datetime.now() - timedelta(seconds=int(match.group(1)))
+	end = fs.getvalue('end')
+	if end.startswith('20'):
+		endyear=int(end[0:4])
+		endmonth=int(end[4:6])
+		endday=int(end[6:8])
+		endhour = endmin = endsec = 0
+		if len(end)>8:
+			endhour = int(end[8:10])
+			if len(end)>10:
+				endmin = int(end[10:12])
+				if len(end)>12:
+					endsec = int(end[12:14])
+		enddate = datetime(endyear, endmonth, endday, endhour, endmin, endsec)
+	match = re.match(r'-([0-9]+)(year|month|week|day|hour|min|sec)', end, re.I)
+	if match:
+		if 'year' in match.group(2):
+			enddate = datetime.now() - timedelta(days=int(match.group(1))*365)
+		if 'month' in match.group(2):
+			enddate = datetime.now() - timedelta(days=int(match.group(1))*31)
+		if 'week' in match.group(2):
+			enddate = datetime.now() - timedelta(weeks=int(match.group(1)))
+		if 'day' in match.group(2):
+			enddate = datetime.now() - timedelta(days=int(match.group(1)))
+		if 'hour' in match.group(2):
+			enddate = datetime.now() - timedelta(hours=int(match.group(1)))
+		if 'min' in match.group(2):
+			enddate = datetime.now() - timedelta(minutes=int(match.group(1)))
+		if 'sec' in match.group(2):
+			enddate = datetime.now() - timedelta(seconds=int(match.group(1)))
 
 #tz = pytz.timezone('Europe/London')
 #startdate=tz.localize(startdate).astimezone(pytz.utc)
@@ -194,9 +194,9 @@ temps, targetTemps = get_temps(headers, id, startdate, enddate)
 currentTemp, currentTarget = get_current_temps(headers, id)
 weather = get_weather(startdate, enddate)
 if startdate.day == enddate.day:
-        xformat="HH:mm:ss"
+	xformat="HH:mm:ss"
 else:
-        xformat="DD-MM-YY HH:mm:ss"
+	xformat="DD-MM-YY HH:mm:ss"
 
 print """
 <html>
@@ -212,18 +212,18 @@ var pagelink=location.origin+location.pathname;
 window.onload = function () {
 
 var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        zoomEnabled: true,
-        zoomType: "xy",
-        legend: {
-                cursor: "pointer",
-        },
-        axisY :{
-                includeZero:false,
-                titleFontSize: 20,
-                titleMaxWidth: 450
-        },
-        data: data
+	animationEnabled: true,
+	zoomEnabled: true,
+	zoomType: "xy",
+	legend: {
+		cursor: "pointer",
+	},
+	axisY :{
+		includeZero:false,
+		titleFontSize: 20,
+		titleMaxWidth: 450
+	},
+	data: data
 });
 
 chart.render();
@@ -250,13 +250,13 @@ print "var dataSeries2 = { type: 'line', xValueType: 'dateTime', showInLegend: t
 print "var dataSeries3 = { type: 'line', xValueType: 'dateTime', showInLegend: true, legendText: 'Weather', xValueFormatString: '"+xformat+"' };"
 
 for i in sorted(temps.keys()):
-        print "dataPoints.push({x: "+str(i)+", y: "+str(temps[i])+" });"
+	print "dataPoints.push({x: "+str(i)+", y: "+str(temps[i])+" });"
 
 for i in sorted(targetTemps.keys()):
-        print "dataPoints2.push({x: "+str(i)+", y: "+str(targetTemps[i])+" });"
+	print "dataPoints2.push({x: "+str(i)+", y: "+str(targetTemps[i])+" });"
 
 for i in sorted(weather.keys()):
-        print "dataPoints3.push({x: "+str(i)+", y: "+str(weather[i])+" });"
+	print "dataPoints3.push({x: "+str(i)+", y: "+str(weather[i])+" });"
 
 print """
 dataSeries.dataPoints = dataPoints;
@@ -286,20 +286,20 @@ function setTemp() {
 	location.reload();
 }
 function boostTemp() {
-        temp = document.getElementById("boostTempToSet").value;
-        time = document.getElementById("boostTime").value;
-        console.log('boost '+temp+' '+time);
-        var data={'nodes': [{'attributes': {'targetHeatTemperature': {'targetValue': temp}, 'activeHeatCoolMode': {'targetValue': 'BOOST'}, 'scheduleLockDuration': {'targetValue': time}}}]}; 
-        $.ajax({
-	        url: nodesurl,
-                type: 'PUT',
-                headers: headers,
-                data: JSON.stringify(data),
-                success: function () {
-                        console.log('success')
-                }
-        });
-        location.reload();
+	temp = document.getElementById("boostTempToSet").value;
+	time = document.getElementById("boostTime").value;
+	console.log('boost '+temp+' '+time);
+	var data={'nodes': [{'attributes': {'targetHeatTemperature': {'targetValue': temp}, 'activeHeatCoolMode': {'targetValue': 'BOOST'}, 'scheduleLockDuration': {'targetValue': time}}}]}; 
+	$.ajax({
+		url: nodesurl,
+		type: 'PUT',
+		headers: headers,
+		data: JSON.stringify(data),
+		success: function () {
+			console.log('success')
+		}
+	});
+	location.reload();
 }
 </script>
 <script src="../canvasjs.min.js"></script>
