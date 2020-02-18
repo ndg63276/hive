0f815c3aae7d1d1d6a12c961b11555e2e57f7325
ndg63276 <mark.williams@diamond.ac.uk>
2020-02-18 13:44:53 +0000

    Make a basic login page

---
+++ cgi-bin/login.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+print("Content-type:application/json")
+print()
+
+from hive import login, get_json
+import cgi
+import json
+import os
+
+
+cred_path = 'cgi-bin/credentials.json'
+if not os.path.isfile(cred_path):
+	j = {}
+else:
+	with open(cred_path, 'r') as f:
+		j = json.load(f)
+
+fs = cgi.FieldStorage()
+
+status = {"status": "failed"}
+
+if 'username' in fs and 'password' in fs:
+	username = fs.getvalue('username')
+	password = fs.getvalue('password')
+	headers = login(username, password)
+
+if 'authorization' in headers:
+	j['username'] = username
+	j['password'] = password
+	with open(cred_path, 'w') as f:
+		json.dump(j, f, indent=4)
+	print(json.dumps(headers))
+else:
+	print(json.dumps(status))
